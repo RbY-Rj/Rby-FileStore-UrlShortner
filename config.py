35accd35582e1334c5ac283e691c1105731081b3
Manicka Raj <96897008+RbY-Rj@users.noreply.github.com>
2025-12-29 03:46:55 +0530

    Change default values of config variables to empty
    Updated environment variable defaults to empty strings.

--- config.py
+++ config.py
@@ -6,34 +6,34 @@ from logging.handlers import RotatingFileHandler
 #Don't Edit Anything [Support @im_piro | @PiroHackz]
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7046087556:AAEXL3cXiHthuk5z-X7CXze7-2e7eU-otO8")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", "10435103"))
+APP_ID = int(os.environ.get("APP_ID", ""))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "c1003f5d2bf7594f6977c6b80f376eb9")
+API_HASH = os.environ.get("API_HASH", "")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002008496197"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "819119516"))
+OWNER_ID = int(os.environ.get("OWNER_ID", ""))
 
 #Port
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://RbyTheatreBot:RbyTheatreBot@cluster0.adpff9m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DATABASE_NAME", "RbyTheatreBot")
+DB_URI = os.environ.get("DATABASE_URL", "")
+DB_NAME = os.environ.get("DATABASE_NAME", "")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001780992747"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", ""))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "tnvalue.in")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "96bc75957ae5589c280f1a794e431a303c32373e")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "")
 
 #start message
 START_MSG = os.environ.get("START_MESSAGE", "𝗛𝗲𝗹𝗹𝗼 {mention}👋  𝗜 𝗛𝗮𝘃𝗲 𝗔𝗹𝗹 #𝗧𝗵𝗲𝗮𝘁𝗿𝗲 𝗠𝗼𝘃𝗶𝗲𝘀  𝗪𝗶𝘁𝗵 𝗔𝗹𝗹 𝗤𝘂𝗮𝗹𝗶𝘁𝗶𝗲𝘀 ... 🔥       «【  𝗙𝗼𝗿 𝗡𝗲𝘄 𝗠𝗼𝘃𝗶𝗲𝘀 ➳ @RbyLinkzz⚡ 】»  𝗦𝗵𝗮𝗿𝗲 𝗔𝗻𝗱 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 ❣️.")
